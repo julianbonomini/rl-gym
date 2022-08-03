f921010af799bf96d992f2a554561a8f38548088
Julian Bonomini <jbonomini@apple.com>
2022-08-03 10:54:34 +0200

    base line

--- evaluate.py
+++ evaluate.py
@@ -1,26 +1,16 @@
-import gym
 import os
+from tabnanny import verbose
+import gym
 from stable_baselines3 import A2C
-import time
 from stable_baselines3.common.vec_env import VecFrameStack
+from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3.common.env_util import make_atari_env
 
 env_name = 'ALE/Breakout-v5'
 env = make_atari_env(env_name, n_envs=1, seed=0)
 env = VecFrameStack(env, n_stack=4)
+
 a2c_path = os.path.join('training', 'saved_models', 'Breakout', 'A2C_Breakout_6m')
 model = A2C.load(a2c_path, env)
 
-observation = env.reset()
-score = 0
-done = [False, False, False, False]
-denzel = False
-# episodes = 5
-# for eposide in range(1, episodes):
-while not denzel:
-    action, _ = model.predict(observation)
-    observation, reward, done, info = env.step(action)
-    score += reward
-    time.sleep(0.1)
-    env.render(mode='human')
-env.close()
+evaluate_policy(model, env, n_eval_episodes=10, render=True)
